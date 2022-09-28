5af5e6867dce74115a8fc09598dbc500ff6e53d4
Erik Russell <erikrussells@gmail.com>
2022-09-28 09:25:55 -0400

    commit tree 6563b805b701eb4ffc61c631707eaaf6f8d36306

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+// const creatingAnElement = {
+//     method: "POST",
+//     header:{
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Sid",
+//         dogBreed: "PitBull",
+//       }),
+
+      
+// }
+
+// fetch("http://localhost:3000/dogs", creatingAnElement)
+// .then(res => res.json)
+
+function submitData(name, email) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headrs: {
+            "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+    })
+    .then(res => res.json())
+    .then(data => document.body.innerHTML = data[id])
+    .catch(error => document.body.innerHTML = error.message)
+    console.lob(data)
+}
